5f06a5d371ac2edf630c2195abeabf71e76dd5aa
guanbofeng <641892857@qq.com>
2022-05-05 13:51:53 +0800

    验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	v1 "goApi/app/http/controllers/api/v1"
 	"goApi/app/models/user"
+	"goApi/app/requests"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,12 +18,8 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析 JSON 请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -35,7 +32,13 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		// 出错了，中断请求
 		return
 	}
-
+	//表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+	}
 	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
