555b2d9c894b34fba29a749ed84fe616d59da6c7
guanbofeng <641892857@qq.com>
2022-05-13 16:05:50 +0800

    限流中间件

--- routes/api.go
+++ routes/api.go
@@ -3,13 +3,23 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"goApi/app/http/controllers/api/v1/auth"
+	"goApi/app/http/middlewares"
 	"net/http"
 )
 
 func RegisterAPIRoutes(r *gin.Engine) {
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
 	{
 		authGroup := v1.Group("/auth")
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
+		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
 			suc := new(auth.SignupController)
 			// 判断手机是否已注册
