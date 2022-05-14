0f33f7db32a0eb2187de22b9b404c4b9627f248f
guanbofeng <641892857@qq.com>
2022-05-15 00:01:04 +0800

    REASDE.md

--- main.go
+++ main.go
@@ -18,43 +18,6 @@ func init() {
 }
 func main() {
 
-	/*	// 配置初始化，依赖命令行 --env 参数
-		var env string
-		flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-		flag.Parse()
-		config.InitConfig(env)
-
-		bootstrap.SetupLogger()
-		// 设置 gin 的运行模式，支持 debug, release, test
-		// release 会屏蔽调试信息，官方建议生产环境中使用
-		// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
-		// 故此设置为 release，有特殊情况手动改为 debug 即可
-		//gin.SetMode(gin.DebugMode)
-
-		// new 一个 Gin Engine 实例
-		router := gin.New()
-		// 初始化 DB
-		bootstrap.SetupDB()
-		// 初始化 Redis
-		bootstrap.SetupRedis()
-		// 初始化路由绑定
-		bootstrap.SetupRoute(router)
-
-		router.GET("/test_auth", middlewares.AuthJWT(), func(c *gin.Context) {
-			userModel := auth.CurrentUser(c)
-			response.Data(c, userModel)
-		})
-		router.GET("/test_guest", middlewares.GuestJWT(), func(c *gin.Context) {
-			c.String(http.StatusOK, "Hello guest")
-		})
-
-		// 运行服务
-		err := router.Run(":" + config.Get("app.port"))
-		if err != nil {
-			// 错误处理，端口被占用了或者其他错误
-			fmt.Println(err.Error())
-		}*/
-
 	// 应用的主入口，默认调用 cmd.CmdServe 命令
 	var rootCmd = &cobra.Command{
 		Use:   config.Get("app.name"),
