679bf1effed33989e1fd37d032605d92b6d05683
guanbofeng <641892857@qq.com>
2022-05-14 13:57:17 +0800

    显示话题

--- routes/api.go
+++ routes/api.go
@@ -71,6 +71,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
 			tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
+			tpcGroup.GET("/:id", tpc.Show)
 		}
 		v1.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
